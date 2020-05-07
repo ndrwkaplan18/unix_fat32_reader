6f0c3e8759de54f7d3ec2ef619e56d6800cac370
ndrwkaplan18 <ndrwkaplan18@gmail.com>
2020-05-06 21:08:08 -0400

    no more memory leaks

--- fat32_reader.c
+++ fat32_reader.c
@@ -39,16 +39,18 @@
 	#define ATTR_LONG_NAME 0x0F
 	#define ENTRY_DELETED 0x40
 	#define ENTRY_IS_LAST 0x00
-	#define SHORT_NAME_FIRST_COMPONENT_MAX_LENGTH 9
-	#define SHORT_NAME_SECOND_COMPONENT_MAX_LENGTH 4
-	#define SHORT_NAME_FULL_NAME_MAX_LENGTH 13
+	#define SHORT_NAME_FIRST_MAX_LENGTH 9
+	#define SHORT_NAME_SECOND_MAX_LENGTH 4
+	#define SHORT_NAME_FULL_MAX_LENGTH 13
 	#define SPACE 0x20
 	#define PERIOD 0x2E
 	#define NEWLINE 0xA
+	#define MAX_ATTR_LEN 83
 /********************************************************************************************/
 /* STRUCT DEFINITIONS */
 /********************************************************************************************/
 	typedef struct {
+		FILE *img_fp;
 		int img_fd;
 		unsigned short BPB_BytsPerSec;
 		unsigned char BPB_SecPerClus;
@@ -73,9 +75,9 @@
 		int first_len;
 		int last_len;
 		int full_len;
-		char first[SHORT_NAME_FIRST_COMPONENT_MAX_LENGTH];
-		char last[SHORT_NAME_SECOND_COMPONENT_MAX_LENGTH];
-		char full_name[SHORT_NAME_FULL_NAME_MAX_LENGTH];
+		char first[SHORT_NAME_FIRST_MAX_LENGTH];
+		char last[SHORT_NAME_SECOND_MAX_LENGTH];
+		char full_name[SHORT_NAME_FULL_MAX_LENGTH];
 		unsigned char attr;
 	} entry_t;
 /********************************************************************************************/
@@ -138,7 +140,8 @@
 	}
 
 	void display_stat(char *name, char only_size){
-		char *input = parse_filename_input(name, 4);
+		char *input;
+		if((input = parse_filename_input(name, 4)) == 0) return;
 
 		pwd_t *wd = &pwd;
 		entry_t *entry = (entry_t*) malloc(sizeof(entry_t));
@@ -148,14 +151,15 @@
 			if(entry->attr == ENTRY_IS_LAST) break;
 			if(entry->attr & ENTRY_DELETED || entry->attr & ATTR_LONG_NAME) continue;
 			if(!strncmp(input, entry->full_name, entry->full_len)){
-				if(!only_size) fprintf(stdout, "Size is %d\nAttributes %s\nNext cluster number is 0x%x, %d\n",entry->size, get_file_attr_type(entry->attr), entry->next_clust, entry->next_clust);
+				char * attrs = get_file_attr_type(entry->attr);
+				if(!only_size) fprintf(stdout, "Size is %d\nAttributes %s\nNext cluster number is 0x%x, %d\n",entry->size, attrs, entry->next_clust, entry->next_clust);
 				else fprintf(stdout, "Size is %d\n", entry->size);
-				free(entry);
+				free(entry); free(input); free(attrs);
 				return;
 			}
 		}
 		fprintf(stderr, "Error: file/directory does not exist\n");
-		free(entry);
+		free(entry); free(input);
 	}
 
 	void display_size(char *name){
@@ -167,12 +171,13 @@
 		pwd_t *wd = &root_dir;
 		read_entry(entry, wd->cluster, 0);
 		if(entry->first_len > 0) fprintf(stdout, "%s%s\n",entry->first, entry->last);
-		else fprintf(stderr, "Error: volume name not found");
+		else fprintf(stderr, "Error: volume name not found\n");
 		free(entry);
 	}
 
 	void do_cd(char *dir_name){
-		char *input = parse_filename_input(dir_name, 2);
+		char *input;
+		if((input = parse_filename_input(dir_name, 2)) == 0) return;
 		pwd_t *wd = &pwd;
 		fatinfo_t *fi = &fat_info;
 		entry_t *entry = (entry_t*) malloc(sizeof(entry_t));
@@ -183,13 +188,13 @@
 			if(entry->attr & ENTRY_DELETED || entry->attr & ATTR_LONG_NAME || !(entry->attr & ATTR_DIRECTORY)) continue;
 			if(!strncmp(input, entry->full_name, entry->full_len)){
 				wd->offset = (entry->next_clust + fi->FirstDataSector) * fi->BPB_BytsPerSec * fi->BPB_SecPerClus;
+				free(wd->cluster); free(entry); free(input);
 				wd->cluster = read_cluster(wd->offset);
-				free(entry);
 				return;
 			}
 		}
 		fprintf(stderr, "Error: directory does not exist\n");
-		free(entry);
+		free(entry); free(input);
 	}
 /********************************************************************************************/
 /* HELPER FUNCTIONS */
@@ -211,7 +216,7 @@
 		}
 		int i, j;
 		// I can't guarantee there are no leftovers from a previous entry, so I need to zero out fullname every time :/
-		for(i = 0; i < SHORT_NAME_FULL_NAME_MAX_LENGTH; i++) entry->full_name[i] = 0;
+		for(i = 0; i < SHORT_NAME_FULL_MAX_LENGTH; i++) entry->full_name[i] = 0;
 		// Next two for loops copy the two components of name
 		// When 0x20 = " " is encountered, we have reached the end of this component
 		for(i = 0; i < 8; i++){
@@ -253,9 +258,9 @@
 
 	unsigned char * read_cluster(off_t offset){
 		fatinfo_t *fi = &fat_info;
-		unsigned char *cluster = malloc(sizeof(unsigned char) * fi->BPB_BytsPerSec * fi->BPB_SecPerClus);
+		unsigned char *cluster = (unsigned char *) malloc(fi->BPB_BytsPerSec * fi->BPB_SecPerClus);
 		lseek(fi->img_fd, offset, SEEK_SET);
-		if(read(fi->img_fd, cluster, fi->BPB_BytsPerSec) < 0) fprintf(stderr, "failed to read file");
+		if(read(fi->img_fd, cluster, fi->BPB_BytsPerSec) < 0) fprintf(stderr, "failed to read file\n");
 		return cluster;
 	}
 
@@ -281,17 +286,19 @@
 	 * @param output return value - parsed argument
 	*/
 	char * parse_filename_input(char *input, int cmd_len){
+		char *output = malloc(MAX_CMD); // just allowing for the user to type in nonsense... I already broke the program doing so :)
 		if(input[cmd_len] != SPACE){
 			fprintf(stderr, "Error: unable to parse args\n");
-			return "";
+			free(output);
+			return 0;
 		}
-		char *output = malloc(SHORT_NAME_FULL_NAME_MAX_LENGTH);
 		// every string will have a \n as its last character
 		int i = 0;
 		while(input[i+1+cmd_len] != NEWLINE){
 			if(i > 11){
 				fprintf(stderr, "Error: file/directory name cannot exceed 12 characters\n");
-				return "";
+				free(output);
+				return 0;
 			}
 			output[i] = input[i+1+cmd_len];
 			// If lowercase, convert to uppercase
@@ -303,7 +310,7 @@
 	}
 
 	char * get_file_attr_type(unsigned char attr){
-		char * attrs = malloc(83); // Just allowing for maximum length of the return string
+		char * attrs = malloc(MAX_ATTR_LEN); // Just allowing for maximum length of the return string
 		if(attr & ATTR_READ_ONLY)	strcat(attrs, "ATTR_READ_ONLY");
 		if(attr & ATTR_HIDDEN)		strcat(attrs, "ATTR_HIDDEN");
 		if(attr & ATTR_SYSTEM)		strcat(attrs, "ATTR_SYSTEM");
@@ -321,7 +328,7 @@
 		pwd_t *root = &root_dir;
 		unsigned char *boot_sector = malloc(BOOT_SECTOR_MAX_BYTES);
 		lseek(fi->img_fd, (off_t) 0, SEEK_SET);
-		if(read(fi->img_fd, boot_sector, BOOT_SECTOR_MAX_BYTES) < 0) fprintf(stderr, "failed to read file");
+		if(read(fi->img_fd, boot_sector, BOOT_SECTOR_MAX_BYTES) < 0) fprintf(stderr, "failed to read file\n");
 		fi->BPB_BytsPerSec = readLittleEnd(boot_sector, BPB_BytsPerSec_offset, sizeof(fi->BPB_BytsPerSec));
 		fi->BPB_SecPerClus = readLittleEnd(boot_sector, BPB_SecPerClus_offset, sizeof(fi->BPB_SecPerClus));
 		fi->BPB_RsvdSecCnt = readLittleEnd(boot_sector, BPB_RsvdSecCnt_offset, sizeof(fi->BPB_RsvdSecCnt));
@@ -332,7 +339,9 @@
 		fi->FirstDataSector = fi->BPB_RsvdSecCnt + fi->BPB_NumFATs * fi->BPB_FATSz32;
 		read_fat();
 		wd->offset = root->offset = fi->FirstDataSector * fi->BPB_BytsPerSec;
-		wd->cluster = root->cluster = read_cluster(fi->FirstDataSector * fi->BPB_BytsPerSec);
+		wd->cluster = read_cluster(fi->FirstDataSector * fi->BPB_BytsPerSec);
+		root->cluster = read_cluster(fi->FirstDataSector * fi->BPB_BytsPerSec);
+		free(boot_sector);
 	}
 
 	void open_img(char *filename){
@@ -341,10 +350,11 @@
 		int fd;
 		fp = fopen(filename, "rb"); //TODO set to write privileges
 		if(fp == NULL){
-			fprintf(stderr, "img file not opened");
+			fprintf(stderr, "img file not opened\n");
 			exit(EXIT_FAILURE);
 		}
 		fd = fileno(fp);
+		fi->img_fp = fp;
 		fi->img_fd = fd;
 	}
 
@@ -352,8 +362,8 @@
 		fatinfo_t *fi = &fat_info;
 		unsigned int size = fi->BPB_FATSz32 * fi->BPB_BytsPerSec;
 		int length = size / sizeof(unsigned int), UIntsPerSec = fi->BPB_BytsPerSec / sizeof(unsigned int);
-		// Yes, read_cluster() calls malloc as well. I need to initialize it here though to silence the compiler about cluster not being initialized.
-		unsigned char * cluster = malloc(fi->BPB_BytsPerSec);
+		// dummy value here to silence the compiler. read_cluster() will do malloc for us.
+		unsigned char * cluster = 0;
 		int i, j = 0;
 		fi->FAT_table = (unsigned int *) malloc(size);
 		/* 
@@ -383,7 +393,7 @@
 			bzero(cmd_line, MAX_CMD);
 			printf("/]");
 			if(!fgets(cmd_line,MAX_CMD,stdin))
-				fprintf(stderr, "error using fgets");
+				fprintf(stderr, "error using fgets\n");
 
 			/* Start comparing input */
 			if(strncmp(cmd_line,"info",4)==0) {
@@ -431,6 +441,10 @@
 
 		/* Close the file */
 		close(fat_info.img_fd);
+		if(fclose(fat_info.img_fp) != EOF) fprintf(stderr, "error closing file pointer\n");
+		free(fat_info.FAT_table);
+		free(pwd.cluster);
+		free(root_dir.cluster);
 		return 0; /* Success */
 	}
 
